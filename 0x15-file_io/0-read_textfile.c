6382bc788c81fa8eb29894a6b0a8505c50467de2
EleashaT <eleashateye1999@gmail.com>
2023-10-30 17:30:39 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads a text file and prints the character
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ * Return: Return number of character printed else 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t rdon, wron;
+	int fn;
+	char *buffer;
+
+	if (!filename)
+		return (0);
+
+	fn = open(filename, O_RDONLY);
+
+	if (fn == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
+		return (0);
+
+	rdon = read(fn, buffer, letters);
+	wron = write(STDOUT_FILENO, buffer, rdon);
+
+	close(fn);
+
+	free(buffer);
+
+	return (wron);
+}
